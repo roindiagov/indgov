c8df341aaa4e1f99debeff339c7cd3360797454d
roindiagov <generaltyhoon1679@gmail.com>
2026-03-19 23:38:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: /indgov/,
+  base: '/indgov/',
   
   server: {
     host: "::",
